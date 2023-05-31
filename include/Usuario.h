1af2162f3f740532045691130552cbfbf889d39b
joaccocaballero <joaquincaballero182@gmail.com>
2023-05-31 18:39:02 -0300

    scaffolding

---
+++ include/Usuario.h
@@ -0,0 +1,15 @@
+
+#include <string>
+using namespace std;
+
+class Usuario {
+    private: 
+        string Nickname;
+        string Contrasenia;
+        string Nombre;
+        string Descripcion;
+    public:
+        Usuario(string Nickname, string Contrasenia, string Nombre, string Descripcion)
+        string obtenerNickname()
+        void Notificar()
+}
