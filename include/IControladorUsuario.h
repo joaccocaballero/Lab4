034a2d5089afb0c0e5fca7f801b61e85d162ea4f
Fati <125610455+fatiga99@users.noreply.github.com>
2023-06-01 15:42:13 -0300

    ctrl U

---
+++ include/IControladorUsuario.h
@@ -0,0 +1,29 @@
+
+#include <string>
+using namespace std;
+#include "DTEstadisticaProfesor.h"
+#include "DTEstadisticaEstudiante.h"
+#include "DTNoticias.h"
+#include "Usuario.h"
+
+class IcontroladorUsuario{
+    virtual void altaProfesor()= 0;
+    virtual boolean confirmarAltaProfesor()= 0;
+    virtual boolean confirmarAltaErofesor()= 0;
+    virtual void ingresarDatosUsuario(string Nickname, string Contrasenia, string Nombre, string Descrpcion)= 0;
+    virtual void ingresarPaisResidencia(string nombre)= 0;
+    virtual set<string> obtenerIdiomasDisponibles()= 0;
+    virtual set<string> obtenerProfesores()= 0;
+    virtual Usuario obtenerProfesor(string Nickname)= 0;
+    virtual set<string> obtenerIdiomasProfesor( )= 0;
+    virtual void agregarEspecializacion(string Nombreidioma);
+    virtual void agregarUsuario()= 0;
+    virtual void ingresarInstituto(string NombreInstituto)= 0;
+    virtual set<string> obtenerEstudiantes()= 0;
+    virtual set<DTEstadisticaEstudiante> obtenerEstadisticaEstudiante(string Nickname)= 0;
+    virtual set<DTEstadisticaProfesor> obtenerEstadisticaProfesor(string Nickname)= 0;
+    virtual set<Idioma> obtenerSubscripcionesPendientes(string Nickname)= 0;
+    virtual set<DTNoticias> obtenerNotificaciones(Nickname: string) = 0;
+    
+    virtual ~IcontroladorUsuario(){};
+}
